a63cd3f5bb1770ba189de405c90abd265ecee968
mpratdev <mpratmaya@gmail.com>
2024-06-26 17:50:40 +0200

    Other changes

---
+++ src/info/brawlers.js
@@ -0,0 +1,266 @@
+export const brawlers = [
+    {
+        brawler: "Shelly",
+        image: "/images/brawlers/Shelly.webp"
+    },
+    {
+        brawler: "Colt",
+        image: "/images/brawlers/Colt.webp"
+    },
+    {
+        brawler: "Bull",
+        image: "/images/brawlers/Bull.webp"
+    },
+    {
+        brawler: "Brock",
+        image: "/images/brawlers/Brock.webp"
+    },
+    {
+        brawler: "Rico",
+        image: "/images/brawlers/Rico.webp"
+    },
+    {
+        brawler: "Spike",
+        image: "/images/brawlers/Spike.webp"
+    },
+    {
+        brawler: "Barley",
+        image: "/images/brawlers/Barley.webp"
+    },
+    {
+        brawler: "Jessie",
+        image: "/images/brawlers/Jessie.webp"
+    },
+    {
+        brawler: "Nita",
+        image: "/images/brawlers/Nita.webp"
+    },
+    {
+        brawler: "Dynamike",
+        image: "/images/brawlers/Dynamike.webp"
+    },
+    {
+        brawler: "El Primo",
+        image: "/images/brawlers/El-Primo.webp"
+    },
+    {
+        brawler: "Mortis",
+        image: "/images/brawlers/Mortis.webp"
+    },
+    {
+        brawler: "Crow",
+        image: "/images/brawlers/Crow.webp"
+    },
+    {
+        brawler: "Bo",
+        image: "/images/brawlers/Bo.webp"
+    },
+    {
+        brawler: "Piper",
+        image: "/images/brawlers/Piper.webp"
+    },
+    {
+        brawler: "Pam",
+        image: "/images/brawlers/Pam.webp"
+    },
+    {
+        brawler: "Tara",
+        image: "/images/brawlers/Tara.webp"
+    },
+    {
+        brawler: "Darryl",
+        image: "/images/brawlers/Darryl.webp"
+    },
+    {
+        brawler: "Penny",
+        image: "/images/brawlers/Penny.webp"
+    },
+    {
+        brawler: "Frank",
+        image: "/images/brawlers/Frank.webp"
+    },
+    {
+        brawler: "Gene",
+        image: "/images/brawlers/Gene.webp"
+    },
+    {
+        brawler: "Tick",
+        image: "/images/brawlers/Tick.webp"
+    },
+    {
+        brawler: "Leon",
+        image: "/images/brawlers/Leon.webp"
+    },
+    {
+        brawler: "Carl",
+        image: "/images/brawlers/Carl.webp"
+    },
+    {
+        brawler: "Rosa",
+        image: "/images/brawlers/Rosa.webp"
+    },
+    {
+        brawler: "Bibi",
+        image: "/images/brawlers/Bibi.webp"
+    },
+    {
+        brawler: "8-Bit",
+        image: "/images/brawlers/8-Bit.webp"
+    },
+    {
+        brawler: "Sandy",
+        image: "/images/brawlers/Sandy.webp"
+    },
+    {
+        brawler: "Emz",
+        image: "/images/brawlers/Emz.webp"
+    },
+    {
+        brawler: "Mr. P",
+        image: "/images/brawlers/Mr-P.webp"
+    },
+    {
+        brawler: "Max",
+        image: "/images/brawlers/Max.webp"
+    },
+    {
+        brawler: "Jacky",
+        image: "/images/brawlers/Jacky.webp"
+    },
+    {
+        brawler: "Gale",
+        image: "/images/brawlers/Gale.webp"
+    },
+    {
+        brawler: "Nani",
+        image: "/images/brawlers/Nani.webp"
+    },
+    {
+        brawler: "Surge",
+        image: "/images/brawlers/Surge.webp"
+    },
+    {
+        brawler: "Colette",
+        image: "/images/brawlers/Colette.webp"
+    },
+    {
+        brawler: "Lou",
+        image: "/images/brawlers/Lou.webp"
+    },
+    {
+        brawler: "Byron",
+        image: "/images/brawlers/Byron.webp"
+    },
+    {
+        brawler: "Edgar",
+        image: "/images/brawlers/Edgar.webp"
+    },
+    {
+        brawler: "Ruffs",
+        image: "/images/brawlers/Ruffs.webp"
+    },
+    {
+        brawler: "Belle",
+        image: "/images/brawlers/Belle.webp"
+    },
+    {
+        brawler: "Squeak",
+        image: "/images/brawlers/Squeak.webp"
+    },
+    {
+        brawler: "Buzz",
+        image: "/images/brawlers/Buzz.webp"
+    },
+    {
+        brawler: "Griff",
+        image: "/images/brawlers/Griff.webp"
+    },
+    {
+        brawler: "Ash",
+        image: "/images/brawlers/Ash.webp"
+    },
+    {
+        brawler: "Meg",
+        image: "/images/brawlers/Meg.webp"
+    },
+    {
+        brawler: "Lola",
+        image: "/images/brawlers/Lola.webp"
+    },
+    {
+        brawler: "Fang",
+        image: "/images/brawlers/Fang.webp"
+    },
+    {
+        brawler: "Eve",
+        image: "/images/brawlers/Eve.webp"
+    },
+    {
+        brawler: "Janet",
+        image: "/images/brawlers/Janet.png"
+    },
+    {
+        brawler: "Bonnie",
+        image: "/images/brawlers/Bonnie.webp"
+    },
+    {
+        brawler: "Otis",
+        image: "/images/brawlers/Otis.webp"
+    },
+    {
+        brawler: "Sam",
+        image: "/images/brawlers/Sam.webp"
+    },
+    {
+        brawler: "Gus",
+        image: "/images/brawlers/Gus.webp"
+    },
+    {
+        brawler: "Buster",
+        image: "/images/brawlers/Buster.webp"
+    },
+    {
+        brawler: "Gray",
+        image: "/images/brawlers/Gray.webp"
+    },
+    {
+        brawler: "Mandy",
+        image: "/images/brawlers/Mandy.webp"
+    },
+    {
+        brawler: "Chester",
+        image: "/images/brawlers/Chester.webp"
+    },
+    {
+        brawler: "R-T",
+        image: "/images/brawlers/R-T.webp"
+    },
+    {
+        brawler: "Willow",
+        image: "/images/brawlers/Willow.webp"
+    },
+    {
+        brawler: "Maisie",
+        image: "/images/brawlers/Maisie.webp"
+    },
+    {
+        brawler: "Hank",
+        image: "/images/brawlers/Hank.webp"
+    },
+    {
+        brawler: "Cordelius",
+        image: "/images/brawlers/Cordelius.webp"
+    },
+    {
+        brawler: "Doug",
+        image: "/images/brawlers/Doug.webp"
+    },
+    {
+        brawler: "Pearl",
+        image: "/images/brawlers/Pearl.webp"
+    },
+    {
+        brawler: "Tara",
+        image: "/images/brawlers/Tara.webp"
+    }
+];
